c0c3261efa23526f822be48a2fae373b5a843842
TienDat <tadat290903@gmail.com>
2024-09-26 23:06:03 +0700

    remove version from docker compose.yml because it is not used now

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,6 +4,7 @@ async function connect(){
     try {
         // mongodb+srv://tadat290903:Khinaobacanli290903@mongoonlinecluster.ghvpe.mongodb.net/
         // mongodb://localhost:27017/
+        // mongodb://mongodb:27017/POS
         await mongoose.connect('mongodb://mongodb:27017/POS')
             .then(() => console.log('Connected!'));
             // console.log("Connected !")
